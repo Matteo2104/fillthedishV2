f79e4963ccae8a471b06e6fd11cb99ab79674120
Matteo2104 <scarcella@outlook.it>
2023-01-02 22:00:29 +0100

    first commit

---
+++ src/test/java/com/example/fillthedish/FillthedishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fillthedish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FillthedishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
